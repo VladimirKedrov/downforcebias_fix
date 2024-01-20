a00d6e87119b2f97969d5751d84842e83cedfcd5
VladimirKedrov <115960462+VladimirKedrov@users.noreply.github.com>
2024-01-20 09:41:05 +0000

    Update config.lua defaults
    Changed defaults to debugging off.

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
 --  Debug ui toggle: 1 for on, 0 for off. "/downforcefix_ui" will toggle ui in game aswell. Also controls client F8 console debug output.
-Config.client_debug = true
+Config.client_debug = false
 -- Enabled server console debug output
-Config.server_debug = true
+Config.server_debug = false
